022e7b039c1403f1741fc557e4258566637f8871
jstrohsnitter <jstrohsnitter@gmail.com>
2025-02-03 12:13:34 -0500

    files working on local

--- src/components/ProjectComponents/ChosenPath.jsx
+++ src/components/ProjectComponents/ChosenPath.jsx
@@ -5,12 +5,12 @@ function ChosenPath() {
 			<h2 className="chosenPathHeader">"Epic Knight IX, Chosen Path: The Chosen Path" DOM Clicker Game</h2>
 			<a style={{display: "table_cell"}} href="https://jstrohsnitter.github.io/dom-game-lab/" target="_blank" rel="noreferrer" className="movieRecsLink">Click this link to view the game</a>
 			<p className="whereToFishP">Here is a simple clicker game I made with Vanilla JS. In this game my aim was to first build a clicker game which was the minimum viable product assigned. In the extra time I had, I began to expand the game making it a choose your own adventure style game, where a user could select a path, add an inventory, and interact with encounters. I got as far as building the store, but you can see that I had a particular focus on styling. The game is fun, has a sense of humor, and involves set-backs when clicking through to keep the user engaged.</p>
-			<img src="/assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.07.54 PM.png" className="chosenPathImg"/>
-            <img src="/assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.08.22 PM.png" className="chosenPathImg"/>
-            <img src="/assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.08.45 PM.png" className="chosenPathImg"/>
-            <img src="/assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.09.27 PM.png" className="chosenPathImg"/>
-            <img src="/assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.09.59 PM.png" className="chosenPathImg"/>
-            <img src="/assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.10.39 PM.png" className="chosenPathImg"/>
+			<img src="./assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.07.54 PM.png" className="chosenPathImg"/>
+            <img src="./assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.08.22 PM.png" className="chosenPathImg"/>
+            <img src="./assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.08.45 PM.png" className="chosenPathImg"/>
+            <img src="./assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.09.27 PM.png" className="chosenPathImg"/>
+            <img src="./assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.09.59 PM.png" className="chosenPathImg"/>
+            <img src="./assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.10.39 PM.png" className="chosenPathImg"/>
 		</div>
         </>
     )
--- src/components/WebDevPortfolio.jsx
+++ src/components/WebDevPortfolio.jsx
@@ -7,15 +7,15 @@ function WebDev({ changePage }) {
 			<h1 className="webDevHeader">Portfolio</h1>
 			<div className="thumbnailDiv">
 				<div className="movieRecsThumb">
-					<img src="./assets/Screen Shot 2025-01-23 at 4.46.01 PM.png" className="mrImage"/>
+					<img src="./assets/movieRecs/Screen Shot 2025-01-23 at 4.46.01 PM.png" className="mrImage"/>
 					<button className="movieRecsButton" onClick={changePage} value="movieRecs">Movie Watchlist App</button>
 				</div>
 				<div className="fishThumb">
-					<img src="/assets/Screen Shot 2025-01-23 at 4.05.11 PM.png" className="fishImage"/>
+					<img src="./assets/Where to fish photos/Screen Shot 2025-01-23 at 4.05.11 PM.png" className="fishImage"/>
 					<button className="whereToFishButton" onClick={changePage} value="whereToFish">Where to Fish App</button>
 				</div>
 				<div className="pathThumb">
-					<img src="/assets/Screen Shot 2025-01-23 at 3.07.54 PM.png" className="pathImage"/>
+					<img src="./assets/Epic Knight Photos/Screen Shot 2025-01-23 at 3.07.54 PM.png" className="pathImage"/>
 					<button className="chosenPathButton" onClick={changePage} value="chosenPath">Chosen Path Game</button>
 				</div>
 			</div>
