f788d921d784072146ffe4edb91b41ce406ef54b
jstrohsnitter <jstrohsnitter@gmail.com>
2025-02-03 11:14:03 -0500

    line 5 config uncommented

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import dns from 'node:dns'
+import dns from 'node:dns'
 
-// dns.setDefaultResultOrder('verbatim')
+dns.setDefaultResultOrder('verbatim')
 
 // https://vite.dev/config/
 export default defineConfig({
