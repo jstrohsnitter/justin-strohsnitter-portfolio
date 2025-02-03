c9b2388cebdf1c7415337d547ca45fb22aaeae1c
jstrohsnitter <jstrohsnitter@gmail.com>
2025-02-03 10:56:42 -0500

    base url change to repo name for image serving

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dns from 'node:dns'
+// import dns from 'node:dns'
 
-dns.setDefaultResultOrder('verbatim')
+// dns.setDefaultResultOrder('verbatim')
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: 'justin-strohsnitter-portfolio',
 })
